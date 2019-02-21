fd00b6ed6ae4251f8aa78e8ca1367f55e56ae3e4
student <student@amarillos-macbook-pro.local>
2019-02-21 16:11:02 -0600

    worked setting up struct to capture data

--- ClassProject/ViewController.swift
+++ ClassProject/ViewController.swift
@@ -10,8 +10,19 @@
 import CoreMotion
 import UIKit
 
+//setting up struct to store accelerometer info
+struct TimeIntervalGoingFast {
+    var startDateTime : Date
+    var endDateTime : Date
+    var averageSpeed : Double
+    var maxSpeed : Double
+    var minSpeed : Double
+    var isUsed : Bool
+}
+
 class ViewController: UIViewController {
-    
+    //initialize struct to array
+    var times : [TimeIntervalGoingFast] = []
     
     //setting up motionManager variable
     let motionManager = CMMotionManager()
@@ -21,10 +32,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var accX: UILabel!
     @IBOutlet weak var accY: UILabel!
     
-    
+    var gaccZ = Double()
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let accelQueue : OperationQueue = OperationQueue()
+        
+        //setting up variables to be measured by app
+        var seconds = 0
+        var speed = 0.0
+        var min = 99999999.0
+        var max = -9999999.0
+        var speedingInstance : TimeIntervalGoingFast = TimeIntervalGoingFast(startDateTime: Date(timeIntervalSinceNow: TimeInterval(0)), endDateTime: Date(timeIntervalSinceNow: TimeInterval(0)), averageSpeed: 0.0, maxSpeed: 0.0, minSpeed: 0.0, isUsed: false)
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
         motionManager.accelerometerUpdateInterval = 1
         motionManager.startAccelerometerUpdates(to: OperationQueue.current!, withHandler: {(accelData: CMAccelerometerData?, errorOC: Error?) in
@@ -35,33 +57,100 @@ class ViewController: UIViewController {
             print(accelData!.acceleration.x)
             self.accX.text = String(accelData!.acceleration.x)
             print(accelData!.acceleration.y)
-            self.accX.text = String(accelData!.acceleration.y)
-        })
+            self.accY.text = String(accelData!.acceleration.y)
+
+            //setting up if statements to start capturing data
+            //https://www.thecalculatorsite.com/conversions/acceleration.php <---used this web site for formula info
+            //showes m/s2 converted to mph/s = conversion rate 1 : 2.2369362920544
+                self.gaccZ = 2.2369362920544 * accelData!.acceleration.z
+            
+                //call motionManager every 1 second to keep running total for speed
+                speed += self.gaccZ
+            
+                //setting max/min speed per tick
+                if (speed > max) {
+                    max = speed
+                }
+                if(speed < min) {
+                    min = speed
+                }
+            
+                //capturing data greater then 10mph/s in struct
+                if (speed > 10.0) {
+                    seconds = seconds + 1
+                    if(speedingInstance.isUsed == false) {
+                        speedingInstance.startDateTime = Date(timeIntervalSinceNow: TimeInterval(0))
+                        speedingInstance.maxSpeed = max
+                        speedingInstance.minSpeed = min
+                        speedingInstance.isUsed = true
+                        
+                    } else {
+                        speedingInstance.endDateTime = Date(timeIntervalSinceNow: TimeInterval(0))
+                        speedingInstance.averageSpeed = speed / Double(seconds)
+                        speedingInstance.minSpeed = min
+                        speedingInstance.maxSpeed = max
+                    }
+                    
+                    //storing struct in array for furture use
+                } else {
+                    if(speedingInstance.isUsed == true) {
+                        //possible fail point appending struct to array
+                        self.times.append(speedingInstance)
+                        speedingInstance.isUsed = false
+                    }
+                }
+        }
+    )
 
         motionManager.startAccelerometerUpdates()
         if let accelerometerData = motionManager.accelerometerData {
         }
         
         print(motionManager)
+        print(seconds)
         
-        //let start = Timer.init(timeInterval: 1.0, repeats: true) { (Timer) in
-           // print("Hi")
-        //}
-        //start.
-        var seconds = 0;
+        //currently removed code not needed will be removed permatley for final version
+/*
         var timerObj : Timer
-        
-        // code for accelerometer conditions AND timrObj == nil
-        timerObj = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+         
+        let start = Timer.init(timeInterval: 1.0, repeats: true) { (Timer) in
             print("Hi")
-            seconds = seconds + 1
         }
-        //else for accel conditions
+         
+         if (accelData!.acceleration.z > 1.0) {
+         self.timerObj = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { (Timer) in
+             print("Hi")
+            seconds = seconds + 1
+         
+           self.gaccZ = self.gaccZ + 1
+         }
+         
+         
+         } else {
+         self.timerObj.invalidate()
+         }
+
+        gaccZ = 0
+        
+        while gaccZ <= 2.2369362920544{
+        // code for accelerometer conditions AND timrObj == nil
+        timerObj = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { (Timer) in
+        print("Hi")
+        seconds = seconds + 1
+            
+        self.gaccZ = self.gaccZ + 1
+        }
+      
+        else for accel conditions
+        timerObj.invalidate() used to stop timer
         timerObj.invalidate()
-        //start.fire()
-        //start.invalidate()
-    }
 
-
+        prints test increments for seconds
+        
+        start.fire()
+        start.invalidate()
+        }
+*/
 }
 
+}
