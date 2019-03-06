bf019e2cc9f8302772c2db850707825fbdf1647c
student <student@amarillos-macbook-pro.local>
2019-03-05 19:44:50 -0600

    Did testing and worked on accuracy

--- ClassProject/ViewController.swift
+++ ClassProject/ViewController.swift
@@ -9,6 +9,7 @@
 //importing coremotion kit
 import CoreMotion
 import UIKit
+import CoreLocation
 
 //setting up struct to store accelerometer info
 struct TimeIntervalGoingFast {
@@ -20,6 +21,15 @@ struct TimeIntervalGoingFast {
     var isUsed : Bool
 }
 
+/*
+func startTest(){
+    var lm = CLLocationManager()
+    var courseSpeed = lm.location?.speed.magnitude
+    var speed = couresSpeed?.magnitude
+    
+}
+*/
+
 class ViewController: UIViewController {
     //initialize struct to array
     var times : [TimeIntervalGoingFast] = []
@@ -27,47 +37,65 @@ class ViewController: UIViewController {
     //setting up motionManager variable
     let motionManager = CMMotionManager()
     
-    //set up labels for testing variables
+    //set up labels for testing variables for accelerometer
     @IBOutlet weak var accZ: UILabel!
     @IBOutlet weak var accX: UILabel!
     @IBOutlet weak var accY: UILabel!
+    @IBOutlet weak var countTimes: UILabel!
+    @IBOutlet weak var speed1: UILabel!
     
     var gaccZ = Double()
+    
+    //var testing = 20.0
    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //starting operation queue
         let accelQueue : OperationQueue = OperationQueue()
         
-        //setting up variables to be measured by app
+        //setting up variables to be measured by app/stored
         var seconds = 0
         var speed = 0.0
+        var x = 0.0
+        var y = 0.0
+        var z = 0.0
+//var speed = testing
         var min = 99999999.0
         var max = -9999999.0
         var speedingInstance : TimeIntervalGoingFast = TimeIntervalGoingFast(startDateTime: Date(timeIntervalSinceNow: TimeInterval(0)), endDateTime: Date(timeIntervalSinceNow: TimeInterval(0)), averageSpeed: 0.0, maxSpeed: 0.0, minSpeed: 0.0, isUsed: false)
         
         
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //starting accelerometer and motionmanager
         motionManager.accelerometerUpdateInterval = 1
         motionManager.startAccelerometerUpdates(to: OperationQueue.current!, withHandler: {(accelData: CMAccelerometerData?, errorOC: Error?) in
             print(accelData!.acceleration.z)
-            
+            print("Testing the intervals")
             //setting up testing for acceleration variables
+            //printing to variables
             self.accZ.text = String(accelData!.acceleration.z)
             print(accelData!.acceleration.x)
+            //self.accZ.text = String(accelData!.acceleration.z)
             self.accX.text = String(accelData!.acceleration.x)
             print(accelData!.acceleration.y)
             self.accY.text = String(accelData!.acceleration.y)
-
+self.countTimes.text = String(self.times.count)
             //setting up if statements to start capturing data
             //https://www.thecalculatorsite.com/conversions/acceleration.php <---used this web site for formula info
             //showes m/s2 converted to mph/s = conversion rate 1 : 2.2369362920544
-                self.gaccZ = 2.2369362920544 * accelData!.acceleration.z
+            x = accelData!.acceleration.x
+            y = accelData!.acceleration.y
+            z = accelData!.acceleration.z
+            //self.gaccZ = 2.2369362920544 * accelData!.acceleration.x
+            self.gaccZ = pow(pow(x, 2) + pow(y, 2) + pow(z, 2), 0.5) * 21.94 - 21.94
             
-                //call motionManager every 1 second to keep running total for speed
-                speed += self.gaccZ
+            //call motionManager every 1 second to keep running total for speed
+                speed = abs(self.gaccZ)
+                self.speed1.text = String(speed)
             
-                //setting max/min speed per tick
+                //setting max/min speed per tick to be recorded
                 if (speed > max) {
                     max = speed
                 }
@@ -75,8 +103,8 @@ class ViewController: UIViewController {
                     min = speed
                 }
             
-                //capturing data greater then 10mph/s in struct
-                if (speed > 10.0) {
+                //capturing data greater then 10mph/s in struct to array
+                if (speed > 1.0) {
                     seconds = seconds + 1
                     if(speedingInstance.isUsed == false) {
                         speedingInstance.startDateTime = Date(timeIntervalSinceNow: TimeInterval(0))
@@ -122,18 +150,22 @@ class ViewController: UIViewController {
  
                         }
                         
-                        
                     }
                 }
         }
     )
 
-        motionManager.startAccelerometerUpdates()
+        //calling accelerometer through motion manager
+        //motionManager.startAccelerometerUpdates()
+        //motionManager.
         if let accelerometerData = motionManager.accelerometerData {
         }
         
+        //testing print calls
         print(motionManager)
         print(seconds)
+        
+//print(times[0])
         
 }
 
